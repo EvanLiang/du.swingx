ea445d2c525ee2eb6084977def044b1cbdbc7781
EvanLiang <EvanLiang@users.noreply.github.com>
2015-01-05 15:22:35 +0800

    Added callback action of close

--- src/main/java/du.swingx/JETabbedPane.java
+++ src/main/java/du.swingx/JETabbedPane.java
@@ -10,153 +10,176 @@ import java.awt.event.MouseEvent;
 
 public class JETabbedPane extends JTabbedPane {
 
-    private boolean closable = false;
+	private boolean closable = false;
 
-    public JETabbedPane() {
-        super();
-    }
+	public JETabbedPane() {
+		super();
+	}
 
-    public JETabbedPane(int tabPlacement) {
-        super(tabPlacement);
-    }
+	public JETabbedPane(int tabPlacement) {
+		super(tabPlacement);
+	}
 
-    public JETabbedPane(int tabPlacement, int tabLayoutPolicy) {
-        super(tabPlacement, tabLayoutPolicy);
-    }
+	public JETabbedPane(int tabPlacement, int tabLayoutPolicy) {
+		super(tabPlacement, tabLayoutPolicy);
+	}
 
-    @Override
-    public void addTab(String title, Icon icon, Component component) {
-        super.addTab(title, icon, component);
-        addClosePanel();
-    }
+	@Override
+	public void addTab(String title, Icon icon, Component component) {
+		super.addTab(title, icon, component);
+		addClosePanel();
+	}
 
-    @Override
-    public void addTab(String title, Icon icon, Component component, String tip) {
-        super.addTab(title, icon, component, tip);
-        addClosePanel();
-    }
+	@Override
+	public void addTab(String title, Icon icon, Component component, String tip) {
+		super.addTab(title, icon, component, tip);
+		addClosePanel();
+	}
 
-    @Override
-    public void addTab(String title, Component component) {
-        super.addTab(title, component);
-        addClosePanel();
-    }
+	@Override
+	public void addTab(String title, Component component) {
+		super.addTab(title, component);
+		addClosePanel();
+	}
 
-    private void addClosePanel() {
-        for (int i = 0; i < this.getTabCount(); i++) {
-            if (!(getTabComponentAt(i) instanceof TabbedPanel)) {
-                TabbedPanel panel = new TabbedPanel(this);
-                panel.setText(getTitleAt(i));
-                setTitleAt(i, "");
-                setTabComponentAt(i, panel);
-            }
-        }
-    }
+	private void addClosePanel() {
+		for (int i = 0; i < this.getTabCount(); i++) {
+			if (!(getTabComponentAt(i) instanceof TabbedPanel)) {
+				TabbedPanel panel = new TabbedPanel(this);
+				panel.setText(super.getTitleAt(i));
+				setTabComponentAt(i, panel);
+			}
+		}
+	}
 
-    private void removeClosePanel() {
-        for (int i = 0; i < this.getTabCount(); i++) {
-            Component c = getTabComponentAt(i);
-            if (c instanceof TabbedPanel) {
-                setTitleAt(i, ((TabbedPanel) c).getText());
-                setTabComponentAt(i, null);
-            }
-        }
-    }
+	private void removeClosePanel() {
+		for (int i = 0; i < this.getTabCount(); i++) {
+			Component c = getTabComponentAt(i);
+			if (c instanceof TabbedPanel) {
+				setTabComponentAt(i, null);
+			}
+		}
+	}
 
-    public boolean isClosable() {
-        return closable;
-    }
+	public boolean isClosable() {
+		return closable;
+	}
 
-    public void setClosable(boolean closable) {
-        this.closable = closable;
-        if (closable) {
-            addClosePanel();
-        } else {
-            removeClosePanel();
-        }
-    }
+	public void setClosable(boolean closable) {
+		this.closable = closable;
+		if (closable) {
+			addClosePanel();
+		} else {
+			removeClosePanel();
+		}
+	}
 
-    class TabbedPanel extends JPanel {
-        private final JTabbedPane tabbedPanel;
-        private JLabel label;
+	public void closeTabAt(int index) {
+		if (index <= -1) {
+			return;
+		}
+		if (tabCloseListener != null
+				&& !tabCloseListener.actionPerformed(index)) {
+			return;
+		}
+		remove(index);
+	}
 
-        public TabbedPanel(final JTabbedPane tabbedPanel) {
-            super(new FlowLayout(FlowLayout.LEFT, 0, 0));
-            this.tabbedPanel = tabbedPanel;
+	private TabCloseListener tabCloseListener;
 
-            setOpaque(false);
-            label = new JLabel();
-            add(label);
-            add(new TabButton());
-            setBorder(BorderFactory.createEmptyBorder(1, 0, 0, 0));
-        }
+	public void setTabCloseListener(TabCloseListener tabCloseListener) {
+		this.tabCloseListener = tabCloseListener;
+	}
 
-        public String getText() {
-            return label.getText();
-        }
+	public interface TabCloseListener {
+		boolean actionPerformed(int index);
+	}
 
-        public void setText(String text){
-            label.setText(text);
-        }
+	class TabbedPanel extends JPanel {
+		private final JTabbedPane tabbedPanel;
+		private JLabel label;
 
-        private class TabButton extends JButton {
-            public TabButton() {
-                setPreferredSize(new Dimension(15, 15));
-                setToolTipText("Close");
-                setUI(new BasicButtonUI());
-                setContentAreaFilled(false);
-                setFocusable(false);
-                setBorderPainted(false);
-                setRolloverEnabled(true);
-                setBorder(null);
-                addMouseListener(new MouseAdapter() {
-                    @Override
-                    public void mouseEntered(MouseEvent e) {
-                        Component component = e.getComponent();
-                        if (component instanceof AbstractButton) {
-                            AbstractButton button = (AbstractButton) component;
-                            button.setBorderPainted(true);
-                        }
-                    }
+		public TabbedPanel(final JTabbedPane tabbedPanel) {
+			super(new FlowLayout(FlowLayout.LEFT, 0, 0));
+			this.tabbedPanel = tabbedPanel;
+			setBorder(BorderFactory.createEmptyBorder(2, 0, 0, 0));
+			setOpaque(false);
+			label = new JLabel();
+			label.setOpaque(true);
+			add(label);
+			add(new TabButton());
+		}
 
-                    @Override
-                    public void mouseExited(MouseEvent e) {
-                        Component component = e.getComponent();
-                        if (component instanceof AbstractButton) {
-                            AbstractButton button = (AbstractButton) component;
-                            button.setBorderPainted(false);
-                        }
-                    }
-                });
-                addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent evt) {
-                        int i = tabbedPanel.indexOfTabComponent(TabbedPanel.this);
-                        if (i != -1) tabbedPanel.remove(i);
-                    }
-                });
-            }
+		public String getText() {
+			return label.getText();
+		}
 
-            @Override
-            public void updateUI() {
-            }
+		public void setText(String text) {
+			label.setText(text);
+		}
 
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                Graphics2D g2 = (Graphics2D) g.create();
-                if (getModel().isPressed()) {
-                    g2.translate(1, 1);
-                }
-                g2.setStroke(new BasicStroke(1));
-                g2.setColor(Color.GRAY);
-                if (getModel().isRollover()) {
-                    g2.setColor(Color.RED);
-                }
-                int delta = 10;
-                g2.drawLine(delta, delta, getWidth() - delta - 1, getHeight() - delta - 1);
-                g2.drawLine(getWidth() - delta - 1, delta, delta, getHeight() - delta - 1);
-                g2.dispose();
-            }
-        }
-    }
+		private class TabButton extends JButton {
+			public TabButton() {
+				setPreferredSize(new Dimension(15, 15));
+				setToolTipText("Close");
+				setUI(new BasicButtonUI());
+				setContentAreaFilled(false);
+				setFocusable(false);
+				setBorderPainted(false);
+				setRolloverEnabled(true);
+				setBorder(null);
+				setOpaque(true);
+				addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseEntered(MouseEvent e) {
+						Component component = e.getComponent();
+						if (component instanceof AbstractButton) {
+							AbstractButton button = (AbstractButton) component;
+							button.setBorderPainted(true);
+						}
+					}
+
+					@Override
+					public void mouseExited(MouseEvent e) {
+						Component component = e.getComponent();
+						if (component instanceof AbstractButton) {
+							AbstractButton button = (AbstractButton) component;
+							button.setBorderPainted(false);
+						}
+					}
+				});
+				addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent evt) {
+						int index = tabbedPanel
+								.indexOfTabComponent(TabbedPanel.this);
+						closeTabAt(index);
+					}
+				});
+			}
+
+			@Override
+			public void updateUI() {
+			}
+
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				Graphics2D g2 = (Graphics2D) g.create();
+				if (getModel().isPressed()) {
+					g2.translate(1, 1);
+				}
+				g2.setStroke(new BasicStroke(1));
+				g2.setColor(Color.GRAY);
+				if (getModel().isRollover()) {
+					g2.setColor(Color.RED);
+				}
+				int delta = 10;
+				g2.drawLine(delta, delta, getWidth() - delta - 1, getHeight()
+						- delta - 1);
+				g2.drawLine(getWidth() - delta - 1, delta, delta, getHeight()
+						- delta - 1);
+				g2.dispose();
+			}
+		}
+	}
 }
